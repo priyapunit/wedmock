9a13445fabf4e5974457f0aebc1e598c23c0ca72
suk c4 <suk c4@ADMINRG-TN51OJ3>
2022-07-27 20:45:38 +0530

    2nd commit

---
+++ src/wedmock/Sample3.java
@@ -0,0 +1,10 @@
+package wedmock;
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/wedmock/Sample4.java
@@ -0,0 +1,10 @@
+package wedmock;
+
+public class Sample4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
