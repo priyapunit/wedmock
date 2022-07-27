bfe46b423927d3eadbf50b064dc2d108c2bfa04a
suk c4 <suk c4@ADMINRG-TN51OJ3>
2022-07-27 20:46:20 +0530

    2 nd commit

--- src/wedmock/Sample3.java
+++ src/wedmock/Sample3.java
@@ -4,7 +4,10 @@ public class Sample3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int x=25,y=5;
+int div;
+div=x/y;
+System.out.println(div);
 	}
 
 }
--- src/wedmock/Sample4.java
+++ src/wedmock/Sample4.java
@@ -4,6 +4,9 @@ public class Sample4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=31,b=2,rem;
+		rem=a%b;
+		System.out.println(rem);
 
 	}
 
