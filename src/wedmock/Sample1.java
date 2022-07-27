e9cd3d006211d3fc4c5bfe83042474f58fb50466
suk c4 <suk c4@ADMINRG-TN51OJ3>
2022-07-27 20:31:48 +0530

    1st commit

---
+++ src/wedmock/Sample1.java
@@ -0,0 +1,13 @@
+package wedmock;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+	int a=10,b=12;
+	int sum;
+	sum =a+b;
+	System.out.println(sum);
+
+	}
+
+}
---
+++ src/wedmock/Sample2.java
@@ -0,0 +1,14 @@
+package wedmock;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p=10,q=2;
+		int mul;
+		mul=p*q;
+		System.out.println(mul);
+
+	}
+
+}
